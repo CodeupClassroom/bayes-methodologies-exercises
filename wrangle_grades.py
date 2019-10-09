39f3087d6b3433eb264201f5e6ea09d7013b7cb2
David Espinola <daver787@gmail.com>
2019-10-09 17:46:00 -0500

    A file for tomorrow's lecture.

---
+++ wrangle_grades.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+def wrangle_grades():
+    grades = pd.read_csv("data/student_grades.csv")
+    grades.drop(columns='student_id', inplace=True)
+    grades.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+    df = grades.dropna().astype('int')
+    return df
