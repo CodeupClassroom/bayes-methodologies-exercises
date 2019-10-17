e156b538ae4a9f7edec5210adee8bf11c0a3b062
David Espinola <daver787@gmail.com>
2019-10-17 11:40:48 -0500

    Fixed notebook to used most updated features_demo.py

--- features_demo.py
+++ features_demo.py
@@ -1,5 +1,6 @@
 from sklearn.feature_selection import SelectKBest, f_regression, RFE
 import statsmodels.api as sm
+import pandas as pd
 from sklearn.linear_model import LassoCV, LinearRegression
 
 
@@ -63,7 +64,7 @@ def ols_backward_elimination(X_train, y_train):
     return selected_features_BE
 
 
-    def optimal_number_of_features(X, y):
+def optimal_number_of_features(X, y):
     '''discover the optimal number of features, n, using our scaled x and y dataframes, recursive feature
     elimination and linear regression (to test the performance with each number of features).
     We will use the output of this function (the number of features) as input to the next function
