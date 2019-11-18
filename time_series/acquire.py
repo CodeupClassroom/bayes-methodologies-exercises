3c84c3076bfec44cba0d4449eac85b58754da212
Maggie Giust <magsgiust@gmail.com>
2019-11-18 11:03:26 -0600

    explore solutions

--- time_series/acquire.py
+++ time_series/acquire.py
@@ -2,6 +2,7 @@ from os import path
 
 import requests
 import pandas as pd
+import env
 
 BASE_URL = 'https://python.zach.lol'
 API_BASE = BASE_URL + '/api/v1'
@@ -51,6 +52,7 @@ def get_store_data(use_cache=True):
     df.to_csv('stores.csv', index=False)
     return df
 
+
 def get_item_data(use_cache=True):
     if use_cache and path.exists('items.csv'):
         return pd.read_csv('items.csv')
@@ -73,6 +75,34 @@ def get_all_data():
     sales = sales.rename(columns={'item': 'item_id', 'store': 'store_id'})
 
     return sales.merge(items, on='item_id').merge(stores, on='store_id')
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def get_store_data_sql(cache=True):
+    csv_file_path = './store_item_demand.csv'
+
+    query = '''
+    SELECT
+        sales.*,
+        items.item_brand,
+        items.item_name,
+        items.item_price,
+        stores.store_address,
+        stores.store_zipcode,
+        stores.store_city,
+        stores.store_state
+    FROM sales
+    JOIN items USING(item_id)
+    JOIN stores USING(store_id)
+    '''
+
+    if cache and path.exists(csv_file_path):
+        return pd.read_csv(csv_file_path)
+    else:
+        df = pd.read_sql(query, get_connection('tsa_item_demand'))
+        df.to_csv(csv_file_path, index=False)
+        return df
 
 def get_opsd_data(use_cache=True):
     if use_cache and path.exists('opsd.csv'):
